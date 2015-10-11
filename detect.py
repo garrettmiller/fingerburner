a9bf07ac821f9a7281c6ab7eef53dc3295a47b5c
axel <axel@Axel-MBP.wv.cc.cmu.edu>
2015-10-11 16:36:36 -0400

    added font_spoof function, but it only detects delimiter for now

--- detect.py
+++ detect.py
@@ -1,4 +1,8 @@
 import re
+from datetime import datetime
+from collections import Counter
+
+#mitmproxy -s detect.py --anticache
 
 fontList = ["Times New Roman", "Copperplate", "Arial", "Calibri", "Sans", "Papyrus",
 "Perpetua", "Gotham", "Serif", "Book Antiqua", "Garamond", "Baskerville",
@@ -10,6 +14,7 @@ regExp = [re.compile(f) for f in fontList]
 #def start(context, argv):
 #	pass
 
+#Handle packet requests
 def request(context, flow):
 	
 	if(flow.request.method != "POST"):
@@ -25,26 +30,56 @@ def request(context, flow):
 	num_match = 0
 	#print(flow.request)
 
-
 	for i in range(len(regExp)):
 		x = regExp[i]
-		f = fontList[i]
 
 		if x.search(flow.request.content):
 			num_match += 1
 			pass
 		pass
 
-	print "matches: ", num_match
 
 	if num_match >= 5:
-		print "Font fingerprinting"
+		print "Font fingerprinting detected"
 		f2 = open ("fp_log.txt", "a")
-		f2.write("%s\n" % flow.request.pretty_url(True))
-		f2.write("%s\n" % flow.request.content)
-		f2.write ("fonts found %d\n" % (num_match))
+		f2.write("----------%s----------\n" % str(datetime.now()))
+		f2.write("URL: %s\n" % flow.request.pretty_url(True))
+		f2.write("CONTENT: %s\n" % flow.request.content)
+		f2.write("FONTS FOUND: %d\n" % (num_match))
 		f2.close()
+		spoofed_content = font_spoof(flow.request.content)
 		pass
 	pass
+
+
+
+def font_spoof(content):
+
+	delimiter_list = []
+
+	#build a list of characters found after a font to find delimiter
+	for i in range(len(regExp)):
+		x = regExp[i]
+		if x.search(content):
+			last_index = content.rfind(x.search(content).group(0))
+			delimiter_list.append(content[last_index + len(x.search(content).group(0))])
+			pass
+		pass
+
+	delimiter_list = Counter(delimiter_list)
+
+	#get the most common character (the delimiter)
+	for key in delimiter_list.most_common(1):
+		print "delimiter is %s\n" % (str(key[0]))
+		delimiter = str(key[0]])
+		pass
+	pass
+
+
+
+	#random.seed()
+	#random.randint(0, len(fontList))
+
+
 
 
