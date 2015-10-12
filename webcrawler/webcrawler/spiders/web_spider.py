aef6469021aa51c62c9badb204f74fa5668784c5
Haley Nguyen <haley.ng@gmail.com>
2015-10-12 08:56:50 -0400

    Add web crawler to detect site that use font fingerprinting

---
+++ webcrawler/webcrawler/spiders/web_spider.py
@@ -0,0 +1,40 @@
+import scrapy
+from webcrawler.items import WebcrawlerItem
+from scrapy.http import Request
+from scrapy.selector.lxmlsel import HtmlXPathSelector
+import re
+
+class WebSpider (scrapy.Spider):
+    name = "webcrawler"
+    start_urls = ["http://www.orbitz.com"]
+    
+    def __init__ (self, pattern=None, *args, **kwargs):
+        super(WebSpider, self).__init__(*args, **kwargs)
+        f = open(pattern, 'r')
+        self.linkPattern = re.compile(f.read())
+        f.close()
+        self.crawledLinks = []
+        pass
+        
+    def parse (self, response):
+        hxs = HtmlXPathSelector(response)
+        links = hxs.select("//a/@href").extract()
+                
+        for link in links:
+            # If it is a proper link and is not checked yet, yield 
+            # it to the Spider
+            if self.linkPattern.match(link) and not link in self.crawledLinks:
+                self.crawledLinks.append(link)
+                yield Request (link, self.parse)
+                pass
+            pass
+        
+        titles = hxs.select('//h1[@class="post_title"]/a/text()').extract()
+        for title in titles:
+            item = WebCrawlerItem()
+            item["title"] = title
+            yield item
+            pass
+        pass
+
+    pass
