a7315090499369e30e15e6af04cd8fa78796323d
Garrett Miller <garrettmiller@gmail.com>
2015-10-13 16:11:48 -0400

    Further code improvements, enabled concurrency, gzip response handling. Untested.

--- detect.py
+++ detect.py
@@ -8,8 +8,10 @@
 ####################################################################
 
 import re
-from datetime import datetime
+from datetime import datetime #Necessary for logging time
 from collections import Counter
+from libmproxy.script import concurrent #Enable concurrency to increase speed
+from libmproxy.protocol.http import decoded #Enable decoding gzipped responses
 
 #Establish a list of fonts to compare against to detect fingerprinting
 fontList = ["Times New Roman", "Copperplate", "Arial", "Calibri", "Sans", "Papyrus",
@@ -18,48 +20,53 @@ fontList = ["Times New Roman", "Copperplate", "Arial", "Calibri", "Sans", "Papyr
 
 regExp = [re.compile(f) for f in fontList]
 
-#Handle packet requests for mitmproxy
+#Handle packet requests for mitmproxy. Runs concurrently
+#for speed, remove @concurrent if this is causing problems.
+@concurrent
 def request(context, flow):
-	
-	#Only run for POST data responses, return otherwise
-	if(flow.request.method != "POST"):
-		return
+	with decoded(flow.request):  # automatically decode gzipped responses.
+		
+		#Only run for POST data responses, return otherwise
+		if(flow.request.method != "POST"):
+			return
 
-	#Write logfile for browsing, likely will remove for deliverable.
-	f1 = open("log.txt", "a")
-	f1.write("%s\n" % (flow.request.pretty_url(True)))
-	f1.write("%s\n" % (flow.request.content))
-	f1.write("%s\n" % (flow.request.method))
-	f1.close()
+		#Always make useragent more common, to reduce fingerprintability.
+		spoofed_content = useragent_spoof(flow.request.headers)
 
-	#DEBUG statement to see flow request
-	#print(flow.request)
+		#Write logfile for browsing, likely will remove this for final deliverable.
+		f1 = open("log.txt", "a")
+		f1.write("%s\n" % (flow.request.pretty_url(True)))
+		f1.write("%s\n" % (flow.request.content))
+		f1.write("%s\n" % (flow.request.method))
+		f1.close()
 
-	#Initialize num_match for iterating through font list
-	num_match = 0
+		#DEBUG statement to see flow request
+		#print(flow.request)
 
-	#Iterate through font list to see if our font was found therein.
-	for i in range(len(regExp)):
-		x = regExp[i]
-		if x.search(flow.request.content):
-			num_match += 1
+		#Initialize num_match for iterating through font list
+		num_match = 0
+
+		#Iterate through font list to see if our font was found therein.
+		for i in range(len(regExp)):
+			if regExp[i].search(flow.request.content):
+				num_match += 1
+				pass
+			pass
+
+		#If we see a lot of words matching fonts in response, 
+		#font fingerprinting is likely happening.
+		#Write to logfile for fingerprinting.
+		if num_match >= 5:
+			print "Font fingerprinting detected"
+			f2 = open ("fp_log.txt", "a")
+			f2.write("----------%s----------\n" % str(datetime.now()))
+			f2.write("URL: %s\n" % flow.request.pretty_url(True))
+			f2.write("CONTENT: %s\n" % flow.request.content)
+			f2.write("FONTS FOUND: %d\n" % (num_match))
+			f2.close()
+			spoofed_content = font_spoof(flow.request.content)
 			pass
 		pass
-
-	#If we see a lot of words matching fonts in response, 
-	#font fingerprinting is likely happening.
-	#Write to logfile for fingerprinting.
-	if num_match >= 5:
-		print "Font fingerprinting detected"
-		f2 = open ("fp_log.txt", "a")
-		f2.write("----------%s----------\n" % str(datetime.now()))
-		f2.write("URL: %s\n" % flow.request.pretty_url(True))
-		f2.write("CONTENT: %s\n" % flow.request.content)
-		f2.write("FONTS FOUND: %d\n" % (num_match))
-		f2.close()
-		spoofed_content = font_spoof(flow.request.content)
-		pass
-	pass
 
 #Function to do font list spoofing as part of a Flash or Java plugin response
 def font_spoof(content):
@@ -69,8 +76,7 @@ def font_spoof(content):
 
 	#Build a list of characters found after a font to find delimiter
 	for i in range(len(regExp)):
-		x = regExp[i]
-		if x.search(content):
+		if regExp[i].search(content):
 			last_index = content.rfind(x.search(content).group(0))
 			delimiter_list.append(content[last_index + len(x.search(content).group(0))])
 			pass
@@ -93,7 +99,18 @@ def font_spoof(content):
 #Function to do useragent spoofing
 #TODO - We'll likely want to find a list of most common ones, set ourselves to that
 #or just minimize the amount of minor version numbers we're sending. 
-def useragent_spoof():
+def useragent_spoof(headers):
+	#Check browser type, then assign to a common version. Placeholders for now.
+	if "Chrome" in str(headers['User-Agent']):
+		headers['User-Agent'] = "CommonChrome"
+	elif "Firefox" in str(headers['User-Agent']):
+		headers['User-Agent'] = "CommonFirefox"
+	elif "Safari" in str(headers['User-Agent']):
+		headers['User-Agent'] = "CommonSafari"
+	elif "MSIE" in str(headers['User-Agent']):
+		headers['User-Agent'] = "CommonIE"
+	
+
 
 
 
